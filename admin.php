6ad68b044cc99ceed9a1ee07df5ec92d3ad59a46
ikermunozjuarez <cfikermunozjuarez@torreroja.cat>
2025-06-02 13:46:48 +0200

    codigo definitivo de login modificado correcto funcionamiento

--- admin.php
+++ admin.php
@@ -18,6 +18,9 @@ if (!isset($_SESSION['usuario'])) {
 <p><a href="crear_usuario.php">Crear nuevo usuario</a></p>
 <p><a href="gestionar_usuarios.php">Gestionar usuarios</a></p>
 <p><a href="logout.php">Cerrar sesión</a></p>
+<a href="crear_producto.php">Crear nuevo producto</a><br>
+<a href="gestionar_productos.php">Gestionar productos</a><br>
+<a href="productos_publicos.php" target="_blank">Ver productos públicos</a><br>
 
 </body>
 </html>
